94db38d5f259f1ce194bd680d964667e1794d55a
VladBrok <vlad.brok99@gmail.com>
2023-09-10 19:07:55 +0300

    calculate hours differently because after 24 they were resetting to 0

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -1,6 +1,8 @@
 export function formatTime(milliseconds: number): string {
   const date = new Date(milliseconds);
-  const hours = date.getUTCHours().toString().padStart(2, '0');
+  const hours = Math.floor(milliseconds / 1000 / 60 / 60)
+    .toString()
+    .padStart(2, '0');
   const minutes = date.getUTCMinutes().toString().padStart(2, '0');
   const seconds = date.getUTCSeconds().toString().padStart(2, '0');
   return `${hours}:${minutes}:${seconds}`;
